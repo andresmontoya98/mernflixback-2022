80604920d093e4f872e804d6841feb6f263e29b5
Andresrock98 <1dandresmontoya@gmail.com>
2022-03-17 09:45:18 +0100

    routes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
+var bookmarksRouter = require('./routes/bookmarks');
 
 var app = express();
 
@@ -26,7 +27,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
+app.use('/bookmarks', bookmarksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
